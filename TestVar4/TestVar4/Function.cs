4c0cbce4705ff3e023ae3f0caeb221d14515b084
Ayhal <54825050+hopeurfnis12@users.noreply.github.com>
2022-11-14 00:34:38 +0900

    testfull

--- TestVar4/TestVar4/Function.cs
+++ TestVar4/TestVar4/Function.cs
@@ -15,5 +15,15 @@ namespace TestVar4
 			else if (b == 5) throw new Exception("ERROR: b == 5");
 			else return (Math.Log(10, Math.Sqrt(a - 5))) / (Math.Pow(b, 3) - 125) * (Math.Pow(x, 5) + 20 * x + 67);
 		}
+
+		public static List<double> GetArray(int n, double x1, double x2, double a, double b)
+		{
+			return 1;
+		}
+
+		public static void Main(String[] args)
+		{
+			GetArray(5, 1, 10, 3, 4);
+		}
 	}
 }
