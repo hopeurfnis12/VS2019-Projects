a24e765996850971ab20de13327c2684104cf357
Ayhal <54825050+hopeurfnis12@users.noreply.github.com>
2023-01-20 15:07:54 +0900

    all

---
+++ TestVar4/TestVar4/UnitTests4.cs
@@ -0,0 +1,275 @@
+﻿namespace TestVar4
+{
+	[TestClass]
+	public class UnitTests4
+	{
+		ListAccur comp = new ListAccur();
+
+		[TestMethod]
+		public void CorrectTestMethod1()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.Start();
+
+			p.StandardInput.WriteLine("2"); // nфффф
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+			p.Close();
+
+			List<double> cor = new List<double>() { 1.5107, 2.3863 };
+			List<double> cur = new List<double>();
+			string path = @"" + name;
+			FileStream fs = new FileStream(path, FileMode.Open);
+			using (StreamReader sr = new StreamReader(fs))
+			{
+				string line;
+				while ((line = sr.ReadLine()) != null) cur.Add(Convert.ToDouble(line));
+			}
+			CollectionAssert.AreEqual(cor, cur, comp);
+			File.Delete(path);
+			Assert.IsFalse(File.Exists(path));
+		}
+
+		[TestMethod]
+		public void CorrectTestMethod2()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+
+			p.Start();
+			p.StandardInput.WriteLine("2"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "C:\\Users\\ayhal\\Documents\\test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+			p.Close();
+
+			List<double> cor = new List<double>() { 1.5107, 2.3863 };
+			List<double> cur = new List<double>();
+			string path = @"" + name;
+			FileStream fs = new FileStream(path, FileMode.Open);
+			using (StreamReader sr = new StreamReader(fs))
+			{
+				string line;
+				while ((line = sr.ReadLine()) != null) cur.Add(Convert.ToDouble(line));
+			}
+			CollectionAssert.AreEqual(cor, cur, comp);
+			File.Delete(path);
+			Assert.IsFalse(File.Exists(path));
+		}
+
+		[TestMethod]
+		public void WrongTestMethod1()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("-1"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("ERROR: n < 1"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod11()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("2"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("3"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("ERROR: a <= 5"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod12()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("2"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("6"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("ERROR: a == 6"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod13()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("2"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("5"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("ERROR: b == 5"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod2()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("1"); // n
+			p.StandardInput.WriteLine("5"); // x1
+			p.StandardInput.WriteLine("2"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "test.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("ERROR: x2 < x1"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod3()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("10"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("10"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "\\or\\o.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("File/path not exists"));
+
+			p.Close();
+		}
+
+		[TestMethod]
+		public void WrongTestMethod4()
+		{
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = "TestirovanieVar4.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.Start();
+			p.StandardInput.WriteLine("1"); // n
+			p.StandardInput.WriteLine("1"); // x1
+			p.StandardInput.WriteLine("10"); // x2
+			p.StandardInput.WriteLine("7"); // a
+			p.StandardInput.WriteLine("8"); // b
+
+			string name = "PRN.txt";
+			p.StandardInput.WriteLine(name);
+
+			p.WaitForExit();
+
+			string s = p.StandardError.ReadToEnd();
+
+			Assert.AreEqual(true, s.Contains("File/path not exists"));
+
+			p.Close();
+		}
+	}
+}
---
+++ TestVar4/TestirovanieVar4/TestirovanieVar4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestirovanieVar4
+{
+	internal class Program
+	{
+		public static double GetF(double x, double a, double b)
+		{
+			if (a <= 5)
+			{
+				throw new Exception("ERROR: a <= 5");
+			}
+			else if (a == 6)
+			{
+				throw new Exception("ERROR: a == 6");
+			}
+			else if (b == 5)
+			{
+				throw new Exception("ERROR: b == 5");
+			}
+			else return (Math.Log(10, Math.Sqrt(a - 5))) / (Math.Pow(b, 3) - 125) * (Math.Pow(x, 5) + 20 * x + 67);
+		}
+
+		public static List<double> GetArray(int n, double x1, double x2, double a, double b)
+		{
+			List<double> garr = new List<double>();
+			if (x2 < x1)
+			{
+				throw new Exception("ERROR: x2 < x1");
+			}
+			else if (n < 1)
+			{
+				throw new Exception("ERROR: n < 1");
+			}
+			else
+			{
+				double h = ((double)x2 - (double)x1) / (n - 1);
+				double x = (double)x1;
+				for (int i = 0; i < n; i++)
+				{
+					garr.Add(GetF(x, a, b));
+					x += h;
+				}
+				return garr;
+			}
+		}
+
+		static void WriteToFile(string name, List<double> val)
+		{
+			if (val.Count() == 0 || val == null) throw new Exception("list error");
+			else
+			{
+				//string path_project = @"..\..\..\" + name;
+				string path = @"" + name;
+				FileStream fs;
+				try
+				{
+					fs = new FileStream(path, FileMode.OpenOrCreate);
+					using (StreamWriter op = new StreamWriter(fs))
+					{
+						for (int i = 0; i < val.Count; i++) op.WriteLine(val[i]);
+					}
+				}
+				catch (IOException)
+				{
+					throw new Exception("File/path not exists");
+				}
+			}
+		}
+
+		public static void Main(String[] args)
+		{
+			Console.Write("n: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("x1: ");
+			double x1 = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("x2: ");
+			double x2 = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("a: ");
+			double a = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("b: ");
+			double b = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("n = " + n
+				+ ", x1 = " + x1 + ", x2 = " + x2
+				+  ", a = " + a + ", b = " + b);
+
+			List<double> otv = GetArray(n, x1, x2, a, b);
+			
+			double h = (x2 - x1) / (n - 1);
+			double x = x1;
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine("x = " + x + ", f(x,a,b) = " + otv[i]);
+				x += h;
+			}
+
+			string name = "";
+			while (name == "")
+			{
+				Console.Write("\nFile name = ");
+				name = Console.ReadLine().ToString();
+				if (name == "") Console.WriteLine("EMPTY NAME!!!");
+			}
+			WriteToFile(name, otv);
+		}
+	}
+}
